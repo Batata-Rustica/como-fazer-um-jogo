9d5dec99221b02f9b2ea602dfab8c41547087d65
Gustavo Oliveira <gugagom1998@gmail.com>
2024-04-24 20:48:59 -0300

    movimento e animação reformulados
    Utilizando Sprite simples e AnimationTree, agora o sistema de movimento e de animação estão mais "matematicamente" condizentes.

---
+++ cenas/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var state_machine
+const speed = 80
+const run_multiplier = 2.5
+const acceleration = 0.2
+
+@export_category("Objects")
+@export var animation_tree: AnimationTree
+
+func _ready():
+	state_machine = animation_tree["parameters/playback"]
+
+func _process(delta):
+	if Input.is_action_pressed("force_exit"): 
+		get_tree().quit()
+		#apertar Ctrl+Q ou Select (joystick) fecha a cena do player
+
+func _physics_process(delta):
+	move()
+	animate()
+	move_and_slide()
+	
+func move():
+	var direction = Input.get_vector("left", "right", "up", "down")
+	var movement = direction.normalized()*speed
+	
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/idle/blend_position"] = direction.normalized()
+		animation_tree["parameters/walk/blend_position"] = direction.normalized()
+		animation_tree["parameters/run/blend_position"] = direction.normalized()
+	
+	if Input.is_action_pressed("shift"):
+		velocity = lerp(velocity,movement*run_multiplier, acceleration)
+	else: velocity = lerp(velocity,movement,acceleration)
+
+func animate():
+	if velocity.length() > 10:
+		if Input.is_action_pressed("shift"):
+			state_machine.travel("run")
+			return
+		else: state_machine.travel("walk")
+		return
+	else: state_machine.travel("idle")
