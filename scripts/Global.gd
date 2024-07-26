be9ffadfa90b134297e3c3202645abc58fc1629c
Gustavo Oliveira <gugagom1998@gmail.com>
2024-07-26 01:55:41 -0300

    .

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,6 +3,10 @@ extends Node
 var music_playing = 1
 var change_music = 0
 var hearts = 3
+var player: CharacterBody2D
+
+var weapon_equipped: bool
+
 
 func brainstorm_entered():
 	change_music = 1
